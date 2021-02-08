add03de01f727e3a237cd2da5f243bd90c2b9c0c
stevenacz <scoailazaa@gmail.com>
2021-02-08 11:23:33 -0500

    Creando un primer Router

---
+++ src/HeroesApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navbar } from './components/ui/Navbar'
+import AppRouter from './routers/AppRouter'
+
+const HeroesApp = () => {
+  return (
+    <AppRouter />
+  )
+}
+
+export default HeroesApp
