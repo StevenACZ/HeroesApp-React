11c398553f5c236a3b3e1c8ce7075d235805eeae
stevenacz <scoailazaa@gmail.com>
2021-02-16 10:53:03 -0500

    Animaciones en nuestro componente

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -31,10 +31,10 @@ const HeroScreen = ({ history }) => {
   return (
     <div className="row mt-5">
       <div className="col-4">
-        <img src={ `../assets/heroes/${ heroeId }.jpg` } className="img-thumbnail" alt={ superhero }/>
+        <img src={ `../assets/heroes/${ heroeId }.jpg` } className="img-thumbnail animate__animated animate__fadeInLeft" alt={ superhero }/>
       </div>
 
-      <div className="col-8">
+      <div className="col-8 animate__animated animate__fadeInRight">
         <h3>{ superhero }</h3>
         <ul className="list-group list-group-flush">
           <li className="list-group-item"> <b>Alter ego: </b> { alter_ego } </li>
